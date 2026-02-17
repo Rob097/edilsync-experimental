6560780f899d6647594e715fc41803a037268363
Roberto Dellantonio <dellantonio47@gmail.com>
2026-02-17 17:20:44 +0000

    File changes

---
+++ src/components/AppProvider.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import { initializeI18n } from '@/components/i18n/i18nConfig';
+import i18next from '@/components/i18n/i18nConfig';
+
+// Initialize i18n on module load
+initializeI18n();
+
+export default function AppProvider({ children }) {
+  return (
+    <I18nextProvider i18n={i18next}>
+      {children}
+    </I18nextProvider>
+  );
+}
